5ee02998c54149c86d8eca8cbd19e08b1fa8cb49
Nina Plevnik <96949730+niny8888@users.noreply.github.com>
2025-04-18 08:58:06 +0200

    push from siht komp

--- Klica/obj/Debug/net8.0/Klica.AssemblyInfo.cs
+++ Klica/obj/Debug/net8.0/Klica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Klica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6704089e3049994dd0d07fdfbe4f30469d27e66f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e8ae5195ba899609f5b7e38f87f733f8e9e27f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Klica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Klica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
