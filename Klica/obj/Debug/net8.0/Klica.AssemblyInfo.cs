86ca048e429d36b4f521e06c17347ebe3b5df1f7
niny8888 <96949730+niny8888@users.noreply.github.com>
2025-04-23 18:17:36 +0200

    b

--- Klica/obj/Debug/net8.0/Klica.AssemblyInfo.cs
+++ Klica/obj/Debug/net8.0/Klica.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Klica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+129cabc8c387f8ff2b52f4172cccd68ab6f5c582")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce33472e509b743d01dd6b6ac9f488e179bc5e7")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Klica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Klica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
